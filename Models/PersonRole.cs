4d3def8689c81dc2f984f97d6d847e48634b8277
Isamu Richmond <richmoni@oregonstate.edu>
2020-06-08 13:55:30 -0700

    Add models

---
+++ Models/PersonRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeoplePro.Models
+{
+    public class PersonRole
+    {
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
